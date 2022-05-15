8ef59c236f87383cfe19dcf4be105ed1440af091
Arianna Blasi <blasia@usi.ch>
2022-05-16 00:29:10 +0100

    Clean script

--- execute-full-JSS-pipeline.sh
+++ execute-full-JSS-pipeline.sh
@@ -1,6 +1,5 @@
 # JSS 2021 Experiments
 
-
 # Get subjects sources and executables
 ./get_evaluation_project_subjects.bash
 
@@ -9,21 +8,18 @@ echo -e "[ cloning RepliComment 2.0 ]\n"
 git clone -q https://github.com/ariannab/replicomment.git
 mv subjects/ replicomment
 cd replicomment/
-
-# Generate RepliComment output folder
-mkdir output
  
 # Assembly RepliComment 2.0 jar with dependencies
 mvn clean compile assembly:single
 
 # Launch RepliComment 2.0 to get outputs to be fed to upDoc
-java -jar target/replicomment-2.0-all.jar org.replicomment.JavadocClonesFinder
+java -cp target/replicomment-2.0-all.jar org.replicomment.JavadocClonesFinder
 cd ..
 
 # Get upDoc 1.0 
- curl -L -o upDoc-1.0-all.jar https://github.com/ariannab/replicomment-JSS21-experiments/releases/download/upDoc1.0/upDoc-1.0-all.jar
+curl -L -o upDoc-1.0-all.jar https://github.com/ariannab/replicomment-JSS21-experiments/releases/download/upDoc1.0/upDoc-1.0-all.jar
 
-# Launch upDoc 1.0 on RepliComment outputs
+# Prepare folders for upDoc and launch upDoc 1.0 on RepliComment outputs
 mkdir JSS-outputs && mkdir JSS-outputs/fields 
 mv replicomment/output replicomment-csv
 
@@ -34,4 +30,3 @@ echo "(expect some 'unexpected empty sentence: []' messages, this is fine too)\n
 java -cp "upDoc-1.0-all.jar" "parser.RepliCmntInterpreter"
 
 echo "\n[ experiment end ]\n"
-
